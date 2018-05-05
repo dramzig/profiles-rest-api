e1a72be896740076b130d1f783ea66b6d470ae54
Donaldo Amaya <damaya@crayonstar.com>
2018-05-05 18:14:00 -0300

    added create, update, partial_update and destroy methods to viewset

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -43,6 +43,7 @@ class HelloApiView(APIView):
 
 class HelloViewSet (viewsets.ViewSet):
     """test API ViewSet"""
+    serializer_class = serializers.HelloSerializer
     def list(self, request):
         """Return a hello message"""
         a_viewset = [
@@ -51,3 +52,24 @@ class HelloViewSet (viewsets.ViewSet):
             'Provides more functionality with less code'
         ]
         return Response({'message':'Hello!','a_viewset':a_viewset})
+    def create(self,request):
+        """Create a new hello message"""
+        serializer = serializers.HelloSerializer(data=request.data)
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}'.format(name)
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def retrieve(self,request, pk=None):
+        """"Retreive an object by ID"""
+        return Response({'http_method':'GET'})
+    def update(self, request, pk=None):
+        """Update and update of an object"""
+        return Response({'http_method':'PUT'})
+    def partial_update(self, request, pk=None):
+        """Partial update"""
+        return Response({'http_method':'PATCH'})
+    def destroy(self, request, pk=None):
+        """Destroy an object"""
+        return Response({'http_method':'DELETE'})
