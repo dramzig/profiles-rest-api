66c95ac819d850afb3cfd19278d225fef9979b39
Donaldo Amaya <damaya@crayonstar.com>
2018-05-08 23:59:16 -0300

    cambio de prueba en las urls

--- src/profiles_project/profiles_project/urls.py
+++ src/profiles_project/profiles_project/urls.py
@@ -19,6 +19,11 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    url(r'^$', core_views.home, name='home'),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^oauth/', include('social_django.urls', namespace='social')),  # <--
+
     url(r'^admin/', admin.site.urls),
     url(r'^api/',include('profiles_api.urls')),
 
