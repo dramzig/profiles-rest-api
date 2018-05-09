275e129cee7a2dc9f8de678fe8ed261ed716b3b8
Donaldo Amaya <damaya@crayonstar.com>
2018-05-09 00:35:13 -0300

    funciona el template de login

--- src/profiles_project/profiles_project/urls.py
+++ src/profiles_project/profiles_project/urls.py
@@ -17,11 +17,8 @@ from django.conf.urls import url
 from django.conf.urls import include
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from mysite.core import views as core_views
-from
 
 urlpatterns = [
-    url(r'^$', core_views.home, name='home'),
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^oauth/', include('social_django.urls', namespace='social')),  # <--
